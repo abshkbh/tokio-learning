40b4456a91cc77a4986609390d330b1044d920a9
Abhishek Bhardwaj <abshkbh@gmail.com>
2022-10-27 17:16:59 -0700

    Listing files working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use tokio::fs::read_dir;
-use tokio::io::Result;
+use std::io;
 use std::path::Path;
+use tokio::fs::read_dir;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let dir_entries = read_dir(Path::new("/tmp")).await?;
+async fn main() -> io::Result<()> {
+    let mut dir_entries = read_dir(Path::new("/tmp/foo")).await?;
+
+    while let Some(dir_entry) = dir_entries.next_entry().await? {
+        println!("{:?}", dir_entry.path())
+    }
     Ok(())
 }

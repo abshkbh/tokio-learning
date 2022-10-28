73ee0e37cec61d32e02bac52ca0d62ee706b2839
Abhishek Bhardwaj <abshkbh@gmail.com>
2022-10-28 16:08:30 -0700

    Implement async copy

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,50 @@
-use std::io;
+use anyhow::{Context, Result};
 use std::path::Path;
-use tokio::fs::read_dir;
+use tokio::fs::{copy, read_dir, DirEntry};
+
+async fn copy_file(src_dir_entry: DirEntry, dest_dir: impl AsRef<Path>) -> Result<()> {
+    let dest_path = dest_dir
+        .as_ref()
+        .to_path_buf()
+        .join(src_dir_entry.file_name());
+
+    println!(
+        "Copying file {:?} to Dir {:?}",
+        src_dir_entry.path(),
+        dest_path
+    );
+
+    let bytes_copied = copy(src_dir_entry.path(), dest_path.as_path())
+        .await
+        .context("Failed to copy file")?;
+
+    println!(
+        "Bytes copied {} file {:?} to Dir {:?}",
+        bytes_copied,
+        src_dir_entry.path(),
+        dest_path
+    );
+
+    Ok(())
+}
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-    let mut dir_entries = read_dir(Path::new("/tmp/foo")).await?;
+async fn main() -> Result<()> {
+    let mut args = std::env::args();
+    // The first arg is the program name. Move ahead.
+    args.next();
+    let src_dir = args.next().context("Source directory not provided")?;
+    let dest_dir = args.next().context("Destination directory not provided")?;
 
-    while let Some(dir_entry) = dir_entries.next_entry().await? {
-        println!("{:?}", dir_entry.path())
+    let mut dir_entries = read_dir(Path::new(&src_dir)).await?;
+
+    let mut handles = Vec::new();
+    while let Some(src_dir_entry) = dir_entries.next_entry().await? {
+        handles.push(tokio::spawn(copy_file(src_dir_entry, dest_dir.clone())));
+    }
+
+    for handle in handles {
+        let _ = handle.await.unwrap().context("Copy failed")?;
     }
     Ok(())
 }
